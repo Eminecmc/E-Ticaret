310e5e1f3f65a3f616f1a4e9a2453691e667c4fc
Eminecmc <eminecmc08@gmail.com>
2021-04-30 09:25:37 +0300

    eticaret

---
+++ ETicaret/ETicaret/Giris/login.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ETicaret.Giris
+{
+    public partial class login : System.Web.UI.Page
+    {
+        SqlConnection baglanti;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+        protected void submit_Click(object sender, EventArgs e)
+        {
+
+            baglanti = new SqlConnection("Data Source=DESKTOP-LV09CUS;Initial Catalog=eticaretvt;Integrated Security=True");
+            baglanti.Open();
+
+            string sifreli = sifrele.MD5Olustur(sifre.Text);
+            SqlCommand komut = new SqlCommand("SELECT * FROM KULLANICILAR1 WHERE UserMail=@P1 AND UserSifre=@P2", baglanti);
+            komut.Parameters.AddWithValue("@P1", email.Text);
+            komut.Parameters.AddWithValue("@P2",sifreli);
+            SqlDataReader dr = komut.ExecuteReader();
+            if (dr.Read())
+            {
+               
+                Session.Add("kullanici", dr["UserID"]);
+                mesaj.Text = Session["kullanici"].ToString();
+                Session.Timeout = 1;
+                Response.Redirect("/site/index.aspx");
+               
+            }
+            else
+            {
+                mesaj.Text="kullanıcı adı veya sifresi hatalı";
+            }
+            baglanti.Close();
+
+        }
+    }
+}
---
+++ ETicaret/ETicaret/Site/sepet.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ETicaret.Site
+{
+    public partial class sepet : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["kullanici"] != null)
+            {
+                string id = Request.QueryString["urunID"];
+                SqlConnection baglanti;
+                baglanti = new SqlConnection("Data Source=DESKTOP-LV09CUS;Initial Catalog=eticaretvt;Integrated Security=True");
+                baglanti.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM UrunListesi WHERE UrunID=" + id, baglanti);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                sepetEkle.DataSource = dt;
+                sepetEkle.DataBind();
+
+                SqlCommand komut = new SqlCommand("INSERT INTO Sepet1(SepetFoto,SepetAdi,SepetFiyat,KullaniciID) values(@foto,@adi,@fiyat,@user)", baglanti);
+                komut.Parameters.AddWithValue("@foto", dt.Rows[0]["UrunFoto"].ToString());
+                komut.Parameters.AddWithValue("@adi", dt.Rows[0]["UrunAdi"].ToString());
+                komut.Parameters.AddWithValue("@fiyat", dt.Rows[0]["UrunFiyat"]);
+                komut.Parameters.AddWithValue("@user", Session["kullanici"]);
+                komut.ExecuteNonQuery();
+                
+            }
+            else
+            {
+                Response.Redirect("/Giris/login.aspx");
+            }
+
+
+        }
+    }
+}
